a75f067f2f7cccf63eb556bc44a9ab2ee4db4e10
Arjun Ambavane <arjun.ambavane@gmail.com>
2025-03-22 11:55:09 +0000

    Add : all todo routes

--- apps/backend/src/controllers/TodoController.ts
+++ apps/backend/src/controllers/TodoController.ts
@@ -1,55 +1,82 @@
 import client from "@repo/db/client";
 
-export const getTodos = async (req:Request) => {
-    try{
-        const userId = (req as {userId?:string}).userId;
-        const user = await client.user.findUnique({where:{id:userId}, include:{todos:true}});
-        if(!user) {
-            return Response.json({
-                type:"error",
-                message:"Error Fetching User Todos",
-            }, {status:500})
-        }
+export const getTodos = async (req: Request) => {
+    try {
+        const userId = (req as { userId?: string }).userId;
+        const user = await client.user.findUnique({ where: { id: userId }, include: { todos: true } });
+        if (!user) return Response.json({ type: "error", message: "Error Fetching User Todos" }, { status: 500 })
+
         const todos = user.todos;
-        if(todos.length === 0){
-            return Response.json({
-                type:"success",
-                message:"User does not have any todos",
-                todos
-            }, {status:200})
-        }
-        return Response.json({
-            type:"success",
-            message:"User Todos",
-            todos,
-        }, {status:200})
-    } catch(e){
-        return Response.json({
-            type:"error",
-            message:"Internal Server Error",
-        }, {status:500})
+        if (todos.length === 0) return Response.json({ type: "success", message: "User does not have any todos", todos }, { status: 200 })
+
+        return Response.json({ type: "success", message: "User Todos", todos, }, { status: 200 })
+    } catch (e) {
+        return Response.json({ type: "error", message: "Internal Server Error" }, { status: 500 })
     }
 }
 
-export const addTodo = async (req:Request) => {
-    try{
-        const userId = (req as {userId?:string}).userId;
+export const addTodo = async (req: Request) => {
+    try {
+        const userId = (req as { userId?: string }).userId as string;
         const body = await req.json();
-        const {title, description} = body as unknown as { title: string, description: string };
-        if(!title || !description) {
-            return Response.json({
-                type:"error",
-                
-            })
+        const { title, description } = body as { title: string, description: string };
+        if (!title || !description) return Response.json({ type: "error", message: "Invalid Todo Body" }, { status: 400 })
+
+        const todo = await client.todo.create({ data: { title, description, completed: false, userId } })
+        return Response.json({ type: "success", message: "Todo Added Successfully", todoId: todo.id }, { status: 200 })
+    } catch (e) {
+        return Response.json({ type: "error", message: "Internal Server Error", }, { status: 500 })
+    }
+}
+
+export const deleteTodo = async (req: Request) => {
+    try {
+        const userId = (req as { userId?: string }).userId as string;
+        const body = await req.json();
+        const { todoId } = body as { todoId: string };
+
+        if (!todoId) return Response.json({ type: "error", message: "Todo id not present" }, { status: 400 })
+
+        try {
+            await client.todo.delete({ where: { id: todoId, userId } });
+            return Response.json({ type: "success", message: "Todo Deleted Successfully", }, { status: 200 });
+        } catch (e) {
+            return Response.json({ type: "error", message: "Todo not found", }, { status: 404 });
         }
+    } catch (e) {
+        return Response.json({ type: "error", message: "Internal Server Error", }, { status: 500 })
+    }
+}
+
+export const updateTodo = async (req: Request) => {
+    try {
+        const userId = (req as { userId?: string }).userId as string;
+        const body = await req.json();
+        const { title, description, completed, todoId } = body as { title: string | null, description: string | null, completed: boolean | null, todoId: string | null };
+        const updateBody = {} as unknown as { title: string, description: string, completed: boolean, todoId: string };
+        if (title) updateBody.title = title;
+        if (description) updateBody.description = description;
+        if (completed) updateBody.completed = completed;
+
+        if (!todoId) return Response.json({ type: "error", message: "Todo id not present" }, { status: 400 });
+        if (Object.keys(updateBody).length === 0) return Response.json({ type: "error", message: "No Valid Keys To update" }, { status: 400 });
+
+        try {
+            await client.todo.update({ where: { id: todoId, userId }, data: updateBody });
+            return Response.json({
+                type: "success",
+                message: "Todo Updated Successfully",
+            }, { status: 200 });
+        } catch (e) {
+            return Response.json({
+                type: "error",
+                message: "Todo not found",
+            }, { status: 404 });
+        }
+    } catch (e) {
         return Response.json({
-            type:"error",
-            message:"Todo Added Successfully",
-        }, {status:200})
-    } catch(e){
-        return Response.json({
-            type:"error",
-            message:"Internal Server Error",
-        }, {status:500})
+            type: "error",
+            message: "Internal Server Error",
+        }, { status: 500 })
     }
 }
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,50 +1,28 @@
 import { serve } from "bun";
-import client from "@repo/db/client";
 import { verify } from 'jsonwebtoken';
 import { handleLogin, handleSignup } from "./controllers/AuthControllers";
-import { addTodo, getTodos } from "./controllers/TodoController";
+import { addTodo, deleteTodo, getTodos, updateTodo } from "./controllers/TodoController";
 
 const authMiddleware = (req: Request, next: (req: Request) => Promise<Response>) => {
     const authHeader = req.headers.get("authorization");
     if (authHeader) {
         const token = authHeader.startsWith('Bearer') ? authHeader.split('Bearer ')[1] : null;
         if (!token) {
-            return Response.json({
-                type: "error",
-                message: "JWT Token Not Present"
-            }, { status: 401 })
+            return Response.json({ type: "error", message: "JWT Token Not Present" }, { status: 401 })
         }
-
         try {
             const payload = verify(token, process.env.JWT_SECRET_KEY as string);
-            if (typeof payload === 'string') {
-                return Response.json({
-                    type: "error",
-                    message: "Invalid JWT Token"
-                }, { status: 401 })
-            }
-            if (!payload || !payload.userId) {
-                return Response.json({
-                    type: "error",
-                    message: "Invalid JWT Token"
-                }, { status: 401 })
-            }
+            if (typeof payload === 'string') return Response.json({ type: "error", message: "Invalid JWT Token" }, { status: 401 });
+            if (!payload || !payload.userId) return Response.json({ type: "error", message: "Invalid JWT Token" }, { status: 401 });
+
             (req as { userId?: string }).userId = payload.userId;
             return next(req);
+            
         } catch (e) {
-            return Response.json({
-                type: "error",
-                message: "Internal Server Error",
-            }, { status: 500 })
-
+            return Response.json({ type: "error", message: "Internal Server Error" }, { status: 500 })
         }
     }
-    else {
-        return Response.json({
-            type: "error",
-            message: "Auth Header Not Present"
-        }, { status: 401 })
-    }
+    else return Response.json({ type: "error", message: "Auth Header Not Present" }, { status: 401 })
 }
 
 const server = serve({
@@ -55,7 +33,9 @@ const server = serve({
         "/login": { POST: handleLogin },
         "/user/todos": {
             GET: (req) => authMiddleware(req, getTodos),
-            POST: (req) => authMiddleware(req, addTodo)
+            POST: (req) => authMiddleware(req, addTodo),
+            DELETE: (req) => authMiddleware(req, deleteTodo),
+            PATCH: (req) => authMiddleware(req, updateTodo)
         }
     }
 })
