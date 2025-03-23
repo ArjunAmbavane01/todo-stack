8385e821158c8dad9333a75eef5e65b5b40e051d
Arjun Ambavane <arjun.ambavane@gmail.com>
2025-03-23 04:28:56 +0000

    Add: native password hashing

--- apps/backend/src/controllers/AuthControllers.ts
+++ apps/backend/src/controllers/AuthControllers.ts
@@ -1,14 +1,12 @@
 import client from "@repo/db/client";
 import {sign} from 'jsonwebtoken';
-import {compare, genSalt, hash} from 'bcrypt';
 
 export const handleSignup = async (req:Request) => {
     try {
         const body = await req.json();
         const { username, password } = body as { username: string, password: string };
 
-        const salt = await genSalt(10);
-        const hashedPassword = await hash(password,salt);
+        const hashedPassword = await Bun.password.hash(password);
         await client.user.create({ data: {username,password:hashedPassword} })
 
         return Response.json({ type: "success", message: "User Signup Successful" }, { status: 200 })
@@ -25,7 +23,7 @@ export const handleLogin = async (req:Request) => {
         const user = await client.user.findFirst({ where: {username} })
         if(!user) return Response.json({ type: "error", message: "User Not Found" }, { status: 404 })
         
-        const verifyPass = await compare(password,user.password);
+        const verifyPass = await Bun.password.verify(password,user.password);
         if(!verifyPass) return Response.json({ type: "error", message: "Wrong Password" }, { status: 401 })
         
         const jwtToken = sign({userId:user.id},process.env.JWT_SECRET_KEY as string)
